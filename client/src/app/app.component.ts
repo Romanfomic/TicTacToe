48c58892af47f32b7a4060a0bf36301dea18a618
Roman Fomichev <romanfomic@gmail.com>
2023-11-04 22:07:14 +0300

    update client
    update
    client.src.app.components.ts
    client.src.app.components.html
    client.src.index.html

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,174 @@
-import { Component } from '@angular/core';
+import { Component, NO_ERRORS_SCHEMA, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit{
+  ngOnInit() {
+    function copyLink() {
+      var linkElement = document.getElementById('link');
+      var divText = linkElement ? linkElement.innerText : '';
+      var textarea = document.createElement('textarea');
+      textarea.value = divText;
+      document.body.appendChild(textarea);
+      textarea.select();
+      textarea.setSelectionRange(0, textarea.value.length);
+      document.execCommand('copy');
+      document.body.removeChild(textarea);
+      alert('Приглашение скопировано');
+    }
+    
+    function getGameId() {
+      const urlSearchParams = new URLSearchParams(window.location.search);
+      const params = Object.fromEntries(urlSearchParams.entries());
+    
+      return params && params['gameId'];
+    }
+    
+    document.addEventListener('DOMContentLoaded', function () {
+      var game = {};
+
+      var boardElement = document.getElementById("board");
+      var remakeElement = document.getElementById("remake");
+      if(boardElement){
+        boardElement.style.display = "none";
+      }
+      if(remakeElement){
+        remakeElement.style.display = "none";
+      }
+    
+      var socket = io();
+    
+      const gameId = getGameId();
+      if (gameId) {
+        socket.emit('join', { gameID: gameId });
+      }
+    
+      document.getElementById("create").addEventListener("click", function () {
+        socket.emit("create");
+        var itemElements = document.getElementsByClassName("item");
+        Array.from(itemElements).forEach(function (element) {
+          element.style.display = "none";
+        });
+      });
+    
+      document.getElementById("remake").addEventListener("click", function () {
+        socket.emit("restart", { id: game.id });
+      });
+    
+      socket.on('created', function (data) {
+        var url = window.location.href + "?gameId=" + data.id;
+        document.getElementById("status").innerHTML = "Отправьте ссылку - приглашение другому игроку";
+        document.getElementById("link").textContent = url;
+        document.getElementById("link").style.display = "inline";
+        document.getElementById("copy-btn").textContent = "Скопировать";
+      });
+    
+      socket.on("start", function (data) {
+        document.getElementById("remake").style.display = "none";
+        var itemElements = document.getElementsByClassName("item");
+        Array.from(itemElements).forEach(function (element) {
+          element.style.display = "none";
+        });
+        document.getElementById("link").style.display = "none";
+    
+        game.id = data.id;
+        game.board = data.gameboard;
+        game.player = data.player;
+    
+        document.getElementById("board").style.display = "block";
+        updateGameboard();
+      });
+    
+      socket.on("failed", function () {
+        document.getElementById("status").innerHTML = "Приглашение не было найдено.";
+        var titleElement = document.getElementsByClassName("title")[0];
+        titleElement.style.display = "none";
+        document.getElementById("create").style.display = "none";
+      });
+    
+      socket.on("invalid", function () {
+        document.getElementById("status").innerHTML = "Not a valid move.";
+      });
+    
+      document.querySelector("table").addEventListener("click", function (e) {
+        var cellClicked = e.target.id;
+    
+        if (game.player.turn === true) {
+          if (game.board[cellClicked] === "") {
+            var data = { id: game.id, cell: cellClicked, player: game.player };
+            socket.emit("move", data);
+          } else {
+            document.getElementById("status").innerHTML = "Нажмите на пустую ячейку.";
+          }
+        }
+      });
+    
+      socket.on("updateGame", function (data) {
+        game.player.turn = !game.player.turn;
+        game.board = data.gameboard;
+        updateGameboard();
+      });
+    
+      socket.on("win", function (data) {
+        game.board = data;
+        updateGameboard();
+        document.getElementById("status").innerHTML = "Вы победили.";
+        document.getElementById("remake").style.display = "block";
+      });
+    
+      socket.on("loss", function (data) {
+        game.board = data;
+        updateGameboard();
+        document.getElementById("status").innerHTML = "Вы проиграли.";
+        document.getElementById("remake").style.display = "block";
+      });
+    
+      socket.on("tie", function (data) {
+        game.board = data.gameboard;
+        updateGameboard();
+        document.getElementById("status").innerHTML = "Tie.";
+        document.getElementById("remake").style.display = "block";
+      });
+    
+      socket.on("quit", function () {
+        document.getElementById("status").innerHTML = "Соединение потеряно.";
+        document.getElementById("board").style.display = "none";
+        var itemElements = document.getElementsByClassName("item");
+        Array.from(itemElements).forEach(function (element) {
+          element.style.display = "block";
+        });
+        var titleElement = document.getElementsByClassName("title")[0];
+        titleElement.style.display = "none";
+        document.getElementById("remake").style.display = "none";
+      });
+    
+      function updateGameboard() {
+        Object.keys(game.board).forEach(function (key) {
+          var value = game.board[key];
+          var element = document.getElementById(key);
+    
+          if (value === "X") {
+            element.classList.remove("red");
+            element.classList.add("blue");
+          } else {
+            element.classList.remove("blue");
+            element.classList.add("red");
+          }
+    
+          element.innerHTML = value;
+        });
+    
+        if (game.player.turn) {
+          document.getElementById("status").innerHTML = "Ваш ход.";
+        } else {
+          document.getElementById("status").innerHTML = "Ходит другой игрок.";
+        }
+      }
+    });
+    
+  }
+  
 }
--- public/client.js
+++ public/client.js
@@ -1,119 +1,154 @@
-$(document).ready(function () {
+function copyLink() {
+    var divText = document.getElementById('link').innerText;
+    var textarea = document.createElement('textarea');
+    textarea.value = divText;
+    document.body.appendChild(textarea);
+    textarea.select();
+    textarea.setSelectionRange(0, textarea.value.length);
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+    alert('Приглашение скопировано');
+  }
+  
+  function getGameId() {
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const params = Object.fromEntries(urlSearchParams.entries());
+  
+    return params && params.gameId;
+  }
+  
+  document.addEventListener('DOMContentLoaded', function () {
     var game = {};
-
-    $("#board").hide();
-    $("#remake").hide();
-
+  
+    document.getElementById("board").style.display = "none";
+    document.getElementById("remake").style.display = "none";
+  
     var socket = io();
-
+  
     const gameId = getGameId();
     if (gameId) {
-        socket.emit('join', { gameID: gameId });
+      socket.emit('join', { gameID: gameId });
     }
-
-    $("#create").on("click", function () {
-        socket.emit("create");
-        $(".item").hide();
+  
+    document.getElementById("create").addEventListener("click", function () {
+      socket.emit("create");
+      var itemElements = document.getElementsByClassName("item");
+      Array.from(itemElements).forEach(function (element) {
+        element.style.display = "none";
+      });
     });
-
-    $("#remake").on("click", function () {
-        socket.emit("restart", { id: game.id });
+  
+    document.getElementById("remake").addEventListener("click", function () {
+      socket.emit("restart", { id: game.id });
     });
-
+  
     socket.on('created', function (data) {
-        const url = window.location.href + "?gameId=" + data.id;
-        $("#status").html(`Отправьте ссылку - прилашение другому игроку`);
-        $("#link").text(url);
-        $('#link').show();
-        $('#copy-btn').text("Скопировать");
+      var url = window.location.href + "?gameId=" + data.id;
+      document.getElementById("status").innerHTML = "Отправьте ссылку - приглашение другому игроку";
+      document.getElementById("link").textContent = url;
+      document.getElementById("link").style.display = "inline";
+      document.getElementById("copy-btn").textContent = "Скопировать";
     });
-
-   
+  
     socket.on("start", function (data) {
-        $("#remake").hide();
-        $(".item").hide();
-        $('#link').hide();
-
-        game.id = data.id;
-        game.board = data.gameboard;
-        game.player = data.player;
-
-        $("#board").show();
-        updateGameboard();
+      document.getElementById("remake").style.display = "none";
+      var itemElements = document.getElementsByClassName("item");
+      Array.from(itemElements).forEach(function (element) {
+        element.style.display = "none";
+      });
+      document.getElementById("link").style.display = "none";
+  
+      game.id = data.id;
+      game.board = data.gameboard;
+      game.player = data.player;
+  
+      document.getElementById("board").style.display = "block";
+      updateGameboard();
     });
-
+  
     socket.on("failed", function () {
-        $("#status").html("Приглашение не было найдено.");
-        $(".title").hide();
-        $("#create").hide();
+      document.getElementById("status").innerHTML = "Приглашение не было найдено.";
+      var titleElement = document.getElementsByClassName("title")[0];
+      titleElement.style.display = "none";
+      document.getElementById("create").style.display = "none";
     });
-
+  
     socket.on("invalid", function () {
-        $("#status").html("Not a valid move.");
+      document.getElementById("status").innerHTML = "Not a valid move.";
     });
-
-    $("table").on("click", function (e) {
-        let cellClicked = e.target.id;
-
-        if (game.player.turn === true) {
-            if (game.board[cellClicked] === "") {
-                let data = { id: game.id, cell: cellClicked, player: game.player };
-                socket.emit("move", data);
-            } else {
-                $("#status").html("Нажмите на пустую ячейку.");
-            }
-        }
-    });
-
-    socket.on("updateGame", function (data) {
-        game.player.turn = !game.player.turn;
-        game.board = data.gameboard;
-        updateGameboard();
-    });
-
-    socket.on("win", function (data) {
-        game.board = data;
-        updateGameboard();
-        $("#status").html("Вы победили.");
-        $("#remake").show();
-    });
-
-    socket.on("loss", function (data) {
-        game.board = data;
-        updateGameboard();
-        $("#status").html("Вы проиграли.");
-        $("#remake").show();
-    });
-    socket.on("tie", function (data) {
-        game.board = data.gameboard;
-        updateGameboard();
-        $("#status").html("Tie.");
-        $("#remake").show();
-    });
-    
-    socket.on("quit", function () {
-        $("#status").html("Соединение потеряно.");
-        $("#board").hide();
-        $(".item").show();
-        $(".title").hide();
-        $("#remake").hide();
-    });
-
-    function updateGameboard() {
-        $.each(game.board, function (key, value) {
-            if (value == "X") {
-                $("#" + key).removeClass("red").addClass("blue");
-            } else {
-                $("#" + key).removeClass("blue").addClass("red");
-            }
-
-            $("#" + key).html(value);
-        });
-
-        if (game.player.turn) {
-            $("#status").html("Ваш ход.");
+  
+    document.querySelector("table").addEventListener("click", function (e) {
+      var cellClicked = e.target.id;
+  
+      if (game.player.turn === true) {
+        if (game.board[cellClicked] === "") {
+          var data = { id: game.id, cell: cellClicked, player: game.player };
+          socket.emit("move", data);
         } else {
-            $("#status").html("Ходит другой игрок.");
+          document.getElementById("status").innerHTML = "Нажмите на пустую ячейку.";
         }
+      }
+    });
+  
+    socket.on("updateGame", function (data) {
+      game.player.turn = !game.player.turn;
+      game.board = data.gameboard;
+      updateGameboard();
+    });
+  
+    socket.on("win", function (data) {
+      game.board = data;
+      updateGameboard();
+      document.getElementById("status").innerHTML = "Вы победили.";
+      document.getElementById("remake").style.display = "block";
+    });
+  
+    socket.on("loss", function (data) {
+      game.board = data;
+      updateGameboard();
+      document.getElementById("status").innerHTML = "Вы проиграли.";
+      document.getElementById("remake").style.display = "block";
+    });
+  
+    socket.on("tie", function (data) {
+      game.board = data.gameboard;
+      updateGameboard();
+      document.getElementById("status").innerHTML = "Tie.";
+      document.getElementById("remake").style.display = "block";
+    });
+  
+    socket.on("quit", function () {
+      document.getElementById("status").innerHTML = "Соединение потеряно.";
+      document.getElementById("board").style.display = "none";
+      var itemElements = document.getElementsByClassName("item");
+      Array.from(itemElements).forEach(function (element) {
+        element.style.display = "block";
+      });
+      var titleElement = document.getElementsByClassName("title")[0];
+      titleElement.style.display = "none";
+      document.getElementById("remake").style.display = "none";
+    });
+  
+    function updateGameboard() {
+      Object.keys(game.board).forEach(function (key) {
+        var value = game.board[key];
+        var element = document.getElementById(key);
+  
+        if (value === "X") {
+          element.classList.remove("red");
+          element.classList.add("blue");
+        } else {
+          element.classList.remove("blue");
+          element.classList.add("red");
+        }
+  
+        element.innerHTML = value;
+      });
+  
+      if (game.player.turn) {
+        document.getElementById("status").innerHTML = "Ваш ход.";
+      } else {
+        document.getElementById("status").innerHTML = "Ходит другой игрок.";
+      }
     }
-});
+  });
